478c845c9230608e3ccb5ac490fab843043afd4f
DucNguyenhuuminh <nguyenhuuminhduc2832005@gmail.com>
2026-02-25 23:53:24 +0700

    test2

--- rpc_server.py
+++ rpc_server.py
@@ -20,12 +20,12 @@ def upload_file(filename: str, enc_data_bin, enc_session_key_bin, signature_bin)
         original_data = encrypt.decrypt_data(enc_data, enc_session_key, SERVER_PRIVATE_KEY)
         is_valid = verify.verify_data(original_data, signature, CLIENT_PUBLIC_KEY)
         if not is_valid:
-            return "ERROR: Invalid signature. File could be fake!"
+            return "ERROR: Invalid signature. File could be fake"
             
         save_path = os.path.join(UPLOAD_DIR, filename)
         with open(save_path, "wb") as f:
             f.write(original_data)
-        return f"SUCCESS: File {filename} have been verified and saved!"
+        return f"SUCCESS: File {filename} have been verified and saved"
         
     except Exception as e:
         return f"ERROR: {str(e)}"
@@ -35,3 +35,7 @@ if __name__ == "__main__":
     print("Server listen at port 9000")
     
     server.register_function(upload_file, "upload_file")
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("\nServer end")
